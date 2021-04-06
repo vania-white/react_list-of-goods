a2923d230cdb486dd6f9aca26ab48f5c7d462b7b
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-04-06 20:32:42 +0300

    Updates

---
+++ precache-manifest.7b2a8c68bc5591e8b08f4b615ff75b84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fa5bca0792058a3181fa74b0c24d25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5d4946472b3155fba92",
+    "url": "./static/css/2.fca3c6c2.chunk.css"
+  },
+  {
+    "revision": "66d8f1e6091116c9876e",
+    "url": "./static/css/main.e78887c7.chunk.css"
+  },
+  {
+    "revision": "d5d4946472b3155fba92",
+    "url": "./static/js/2.4355feeb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4355feeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d8f1e6091116c9876e",
+    "url": "./static/js/main.91a1b6de.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
